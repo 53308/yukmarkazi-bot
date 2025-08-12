2899056bc0da73225009cf5a15c68c49924ed56f
53308 <5330897@gmail.com>
2025-08-12 22:31:48 +0500

    railway_deploy.py

--- railway_deploy.py
+++ railway_deploy.py
@@ -878,3 +878,7 @@ if __name__ == '__main__':
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+    @app.route('/ping')
+def ping():
+    return "pong", 200
